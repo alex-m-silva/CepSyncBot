7ceb3251a3a8a3f6146b1c97b765100955066cef
Alex Matias <alexmatias.am162@gmail.com>
2025-08-04 21:53:52 -0300

    feat: adição do projeto Application e correções nos métodos do repositório em Infrastructure

---
+++ Application/Services/AddressService.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Iterfaces.Repositories;
+using Domain.Iterfaces.Services;
+using Domain.Models;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressService(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public Task AddAsync(List<AddressModel> addresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AddressModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddressModel> GetZipCodeAsync(string robot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDataAsync(AddressModel address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Infrastructure/Repositories/AddressRepository.cs
+++ Infrastructure/Repositories/AddressRepository.cs
@@ -2,45 +2,43 @@
 using Domain.Iterfaces.Repositories;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EFCore.BulkExtensions;
 
 namespace Infrastructure.Repositories
 {
     public class AddressRepository : IAddressRepository
     {
-        private readonly AppDbContext _ctx = new();
+        private readonly AppDbContext _ctx;
         public AddressRepository(AppDbContext ctx)
         {
             _ctx = ctx;
         }
+
         public async Task<List<Address>> GetAllAsync()
         {
             return await _ctx.Addresses.ToListAsync();
         }
         public async Task AddAsync(List<Address> addresses)
         {
-            await _ctx.Addresses.AddRangeAsync(addresses);
+            await _ctx.BulkInsertAsync(addresses);
         }
 
         public async Task<Address?> GetByIdAsync(int id)
         {
-            return await
-                _ctx.Addresses.FirstOrDefaultAsync(x => x.Id == id);
+            return await _ctx.Addresses.FirstOrDefaultAsync(x => x.Id == id);
         }
-
         public async Task<Address?> GetZipCodeAsync(string robot)
         {
-            return await
-                    _ctx.Addresses.FirstOrDefaultAsync(x => x.Robo == robot);
+            return await _ctx.Addresses.FirstOrDefaultAsync(x => 
+            (x.Robo == robot && x.Status == Domain.Enums.EnumStatus.EmAndamento)
+            ||
+            (string.IsNullOrEmpty(x.Robo) && x.Status == Domain.Enums.EnumStatus.Aberto));
         }
 
-        public Task<bool> UpdateDataAsync(Address address)
+        public async Task<bool> UpdateDataAsync(Address address)
         {
-            throw new NotImplementedException();
+            _ctx.Addresses.Update(address);
+            return await _ctx.SaveChangesAsync() > 0;
         }
     }
 }
